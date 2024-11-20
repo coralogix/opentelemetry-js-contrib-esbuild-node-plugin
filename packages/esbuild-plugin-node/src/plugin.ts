d7c1f72bd06e3321b2223216da429781019d0053
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2024-11-20 10:40:44 +0100

    Handle multiple instrumentation modules with the same name (affects mongodb, winston, socket.io)

--- packages/esbuild-plugin-node/src/plugin.ts
+++ packages/esbuild-plugin-node/src/plugin.ts
@@ -127,7 +127,6 @@ export function openTelemetryPlugin(
                 extractedModule.path || ''
               ),
               moduleVersion: pluginData.moduleVersion,
-              instrumentationName: pluginData.instrumentationName,
               oTelInstrumentationClass: config.oTelInstrumentationClass,
               oTelInstrumentationPackage: config.oTelInstrumentationPackage,
               oTelInstrumentationConstructorArgs:
